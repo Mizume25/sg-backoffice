bb513b56c9b12a879936413c1540c6d6fcfd8ad1
Mizume25 <gabrielnivicela649@gmail.com>
2026-06-16 12:44:35 +0200

    Store mejorado , varables reactivas modificadas fix: Aun falta arreglar refresh de datos

--- app/stores/useCateogiresStore.ts
+++ app/stores/useCateogiresStore.ts
@@ -2,14 +2,23 @@
 export const useCategoriesStore = defineStore('categories', () => {
 
     /** Variable general */
-    const { data:allcategories , refresh} = useCategoriesApi();
+    const allcategories = ref<CategoryRecord[]>([]);
 
+    /** Setear Valores */
+    const setCategories = (categories: CategoryRecord[]) => {
+        allcategories.value = categories;
+    }
 
-    const refreshCategories = async () => await refresh();
+    /** Refrescar datos */
+    const refreshCategories = async () => {
+        const data = await $fetch<CategoryRecord[]>('/api/categories')
+        setCategories(data)
+    }
+
 
     /** Funcion para obtener categoria */
-    const findCategory = (id : string) : CategoryRecord | null => {
-        
+    const findCategory = (id: string): CategoryRecord | null => {
+
         if (!allcategories.value.length || !id) return null;
 
         return allcategories.value.find((p) => p.id == id) ?? null;
@@ -17,23 +26,18 @@ export const useCategoriesStore = defineStore('categories', () => {
     }
 
     /** Obtener solo listado padre */
-    const getParents = () : CategoryRecord [] | [] => {
-        
-        if(!allcategories.value.length) return [];
-
-        return allcategories.value.filter((p) => p.parent_id === null);
-    }
+    const parents = computed(() => allcategories.value.filter((p) => p.parent_id === null));
 
 
     /** Obtener padre especifico a partir del id */
-    const getParent = (id : string | null |undefined) : CategoryRecord | undefined =>  {
-        if(!id || !allcategories ) return undefined;
+    const getParent = (id: string | null | undefined): CategoryRecord | undefined => {
+        if (!id || !allcategories) return undefined;
 
         return allcategories.value.find((p) => p.id === id) ?? undefined;
     }
 
 
-    return { allcategories, findCategory, getParents, getParent , refreshCategories };
+    return { allcategories, findCategory, parents, getParent, setCategories, refreshCategories };
 
-    
+
 })
